1a3a17928750b189e0891839bf7d483e01294ca5
Matt Laporte <matt.l.laporte@gmail.com>
2016-03-29 17:42:14 -0400

    Refactored more functional.

--- muser.py
+++ muser.py
@@ -5,30 +5,45 @@ Machine learning music
 import numpy as np
 from scipy.integrate import trapz
 from scipy.io import wavfile
+import tensorflow as tf
 from pylab import plot, show 
 
-# import contents of wav file
-sf, snd = wavfile.read('op28-20.wav')
+def f(t):
+    try:
+        return [int(p * sf) for p in t]
+    except TypeError:
+        return int(t * sf)
 
-# scale signal from -1 to 1
-snd = snd/(2.**15) if snd.dtype == np.dtype('int16') else snd/(2.**31)
-
+def scale_wav(snd, factor=None):
+    # scale signal from -1 to 1
+    if factor:
+        return snd/factor
+    else:
+        return scale_wav(snd, 2.**15 if snd.dtype == np.dtype('int16') else 2.**31) 
+        
 # x = snd.shape[0]             # number of sample points (duration * rate)
 # t = np.arange(0, x, 1) / sf  # sample points as time points (seconds)
 
-# time interval containing the frames for FFT
-i = int(0.5 * sf)
-f = int(1.0 * sf)
+def local_fft(chs, f0, f1, norm="ortho"):
+    # calculate FFTs for each channel over the interval
+    return np.stack(np.fft.fft(ch[f0:f1], norm=norm) for ch in chs)
 
-# calculate FFTs for each channel over the interval
-ws = np.stack(np.fft.fft(ch[i:f], norm="ortho") for ch in snd.T)
-ws_a = abs(ws)
-ws_p = ws_a**2
-fr_a = abs(np.fft.fftfreq(f-i) * sf)   # Hz
+# import contents of wav file
+sf, snd = wavfile.read('op28-20.wav')
+snd = scale_wav(snd)
 
-fr_max = 5000 # Frequency cutoff
-crop = [(fr, ws_p[0][i]) for i, fr in enumerate(fr_a) if fr < fr_max]
-plot(*zip(*crop))
+t0, t1 = (0.5, 1.0)
+w = local_fft(snd.T, f(t0), f(t1))
+
+n = w.shape[1] // 2 + 1
+w_a = abs(w)[:, 0:n]
+w_p = w_a ** 2 
+frqs = np.fft.fftfreq(f(t1) - f(t0))
+frqs_a = abs(sf * frqs)[0:n]   # Hz
+
+frq_max = 5000 # Frequency cutoff
+cut = np.where(frqs_a > frq_max)[0][0] 
+plot(frqs_a[0:cut], w_a[0][0:cut])
 show()
 
-print(sum(ws_p[0]))
+# print(sum(ws_p[1]))
