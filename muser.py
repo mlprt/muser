fe8b2b15d6a9a89fc198b0b6731ba210bdfdcd2d
Matt Laporte <matt.l.laporte@gmail.com>
2016-03-29 20:33:52 -0400

    Added basic JACK client manager class

--- muser.py
+++ muser.py
@@ -5,9 +5,25 @@ Machine learning music
 import numpy as np
 from scipy.integrate import trapz
 from scipy.io import wavfile
+import jack
 import tensorflow as tf
 from pylab import plot, show 
 
+class MuserJACK():
+    """ Manages a JACK client for Muser """
+    def __init__(self, midi_ins=1, midi_outs=1, name="MuserClient"):
+        self.client = jack.Client(name)
+        self.midi_in = [client.midi_inports.register("midi_in_{}".format(j)) for j in range(midi_ins)]
+        self.midi_out = [client.midi_outports.register("midi_out_{}".format(k)) for k in range(midi_outs)]
+        activate()
+    
+    def activate(self):
+        client.activate()
+    def deactivate(self):
+        client.deactivate()
+    def close():
+        client.close()
+        
 def f(t):
     try:
         return [int(p * sf) for p in t]
@@ -35,15 +51,15 @@ snd = scale_wav(snd)
 t0, t1 = (0.5, 1.0)
 w = local_fft(snd.T, f(t0), f(t1))
 
-n = w.shape[1] // 2 + 1
-w_a = abs(w)[:, 0:n]
-w_p = w_a ** 2 
+n = (w.shape[1] // 2) + 1
+w_a = abs(w)[:][0:n]
+w_pwr = w_a ** 2 
 frqs = np.fft.fftfreq(f(t1) - f(t0))
 frqs_a = abs(sf * frqs)[0:n]   # Hz
 
 frq_max = 5000 # Frequency cutoff
 cut = np.where(frqs_a > frq_max)[0][0] 
-plot(frqs_a[0:cut], w_a[0][0:cut])
+plot(frqs_a[0:cut], w_pwr[0][0:cut])
 show()
 
 # print(sum(ws_p[1]))
