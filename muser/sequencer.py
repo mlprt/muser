4f61b2cf32864195d308165080f2b6ab1644620b
Matt Laporte <matt.l.laporte@gmail.com>
2016-06-21 14:59:47 -0400

    Fixed chord generation through `random_pitch_vector()`

--- muser/sequencer.py
+++ muser/sequencer.py
@@ -3,21 +3,23 @@
 import music21
 import numpy as np
 
+PITCH_LO = 0
+PITCH_HI = 127
 VELOCITY_LO = 0
 VELOCITY_HI = 127
-
+""" Absolute limits of MIDI pitch and velocity. """
 PIANO_LO = 21
 PIANO_HI = 108
-""" MIDI pitch range of 88-key piano """
+""" MIDI pitch range of 88-key piano. """
 
 def notation_to_notes(notation):
     """ Parse a notation string and return a list of Note objects.
 
     Parameters:
-        notation (str):
+        notation (str): Melody notated in a `music21`-parsable string format.
 
     Returns:
-        notes (list):
+        notes (list): Sequence of `music21` Note objects.
     """
     sequence = music21.converter.parse(notation)
     notes = list(sequence.flat.getElementsByClass(music21.note.Note))
@@ -29,13 +31,13 @@ def random_note(pitch=None, pitch_range=None, velocity=None, velocity_range=None
     """ Return a random `music21` Note object.
 
     Parameters:
-        pitch (int or str): Defined pitch. Randomly assigned if `None` (default)
-        pitch_range (tuple): Range of MIDI pitches for random assignment
-        velocity (int): Defined velocity. Randomly assigned if `None` (default)
-        velocity_range (tuple): Range of velocities for random assignment
+        pitch (int or str): Defined pitch. Randomly assigned if `None` (default).
+        pitch_range (tuple): Range of MIDI pitches for random assignment.
+        velocity (int): Defined velocity. Randomly assigned if `None` (default).
+        velocity_range (tuple): Range of velocities for random assignment.
 
     Returns:
-        note (music21.note.Note): Note object with assigned properties
+        note (music21.note.Note): Note object with assigned properties.
     """
 
     if pitch is None:
@@ -95,5 +97,23 @@ def note_to_pitch_vector(note, pitch_range=None):
     """ Return a MIDI pitch vector for a `music21` Note object. """
     chord = music21.chord.Chord([note])
     pitch_vector = chord_to_pitch_vector(chord, pitch_range=pitch_range)
+
+    return pitch_vector
+
+
+def random_pitch_vector(pitches, pitch_range=None, velocity=None,
+                        velocity_range=None):
+    """ Return a random pitch vector (chord).
+
+    Parameters:
+        pitches (int): Number of pitches in the chord/vector.
+        velocity (int): Constant velocity of resulting chord. Random if `None`.
+        velocity_range (tuple): Range for random assignment of velocity.
+    Returns:
+        pitch_vector (np.ndarray): Vector specifying pitches
+    """
+    chord = random_chord(chord_size=pitches, pitch_range=pitch_range,
+                         velocity=velocity, velocity_range=velocity_range)
+    pitch_vector = chord_to_pitch_vector(chord)
 
     return pitch_vector
