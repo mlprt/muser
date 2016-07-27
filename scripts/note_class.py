7500202ce4151d98c383b62303c2552e2df56094
Matt Laporte <matt.l.laporte@gmail.com>
2016-07-27 12:31:38 -0400

    Calculate average RFFTs per chord channels, excluding quiet buffers. Added module docstring.

--- scripts/note_class.py
+++ scripts/note_class.py
@@ -1,4 +1,10 @@
-""""""
+"""Experiment to train model to estimate velocity vector from average FFT.
+
+Relies on the file ``params.json`` produced at the beginning of execution of
+chord_batches.py. If a certain number of batches are expected based on the
+``params.json``, but the corresponding dumps don't exist yet, this script
+checks periodically until the next dump is available.
+"""
 
 import json
 import time
@@ -17,7 +23,10 @@ with open(params_file, 'r') as params_file:
 globals().update(params)
 
 clfft = fft.get_cl_fft(axes=(1,))
+batch_vectors = np.ndarray([batch_size, 128], dtype=np.float32)
 batch_ffts = np.ndarray([batch_size], dtype=object)
+batch_rfft_avg = np.ndarray([batch_size, 2, 513], dtype=np.float32)
+rfft_rel_thres = 0.01
 
 # define model
 
@@ -26,8 +35,14 @@ for i_batch in range(batches):
         try:
             batch = np.load(paths['pickle'].format(i_batch))
             break
-        except FileNotFoundError:
-            time.sleep(5)
+        except (FileNotFoundError, OSError):
+            time.sleep(10)
     for i_chord, chord in enumerate(batch):
+        batch_vectors[i_chord, :] = chord['velocity_vector']
         captures = chord['captured_buffers'].astype(np.complex64)
         batch_ffts[i_chord] = fft.fft1d_collapse(captures, fft=clfft)
+        chord_rffts = abs(batch_ffts[i_chord])[:, :, : blocksize // 2 + 1]
+        avg_rffts = np.average(chord_rffts, axis=2)
+        exclude = np.where(avg_rffts < np.max(avg_rffts) * rfft_rel_thres)
+        chord_rfft_avg = np.mean(np.delete(chord_rffts, exclude, 1), 1)
+        batch_rfft_avg[i_chord, :, :] = chord_rfft_avg
