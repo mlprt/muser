90489dfbabb03666c26af4519073c93c9641e4e9
Matt Laporte <matt.l.laporte@gmail.com>
2016-11-10 20:09:42 -0500

    Improved comments and some minor repairs.

--- scripts/midi_play.py
+++ scripts/midi_play.py
@@ -3,35 +3,42 @@
 Only accounts for note on and off events and tempo changes.
 """
 
+import cProfile
+import os
+import pstats
 import midi
-import muser.audio as audio
-import muser.live as live
-import muser.sequencer as sequencer
 import numpy as np
 import scipy.io.wavfile as wavfile
 
-synth_name = 'Pianoteq55'
+import muser.audio as audio
+import muser.live as live
+import muser.sequencer as sequencer
 
-midi_file = '/home/mll/data/midi/goldberg-aria.mid'
+# configuration of synthesizer and I/O paths
+synth_name = 'Pianoteq55'
+midi_name = 'chopin-op28-no7'
+midi_file = '/home/mll/data/midi/{}.mid'.format(midi_name)
+wavfile_path = '/tmp/muser/{}.wav'.format(midi_name)
+profile_path = '/tmp/muser/tmp/profile'
+os.makedirs(profile_path, exist_ok=True)
+
+# basic velocity (volume) bias for beats in a measure
+bias_beats = True
+beat_biases = {(4, 4): (1.0, 0.9, 0.95, 0.875),
+               (3, 4): (1.0, 0.8, 0.9)}
+
+# read and flatten MIDI file
 midi_pattern = midi.read_midifile(midi_file)
 ticks_per_beat = midi_pattern.resolution
-
-# flatten tracks
 midi_pattern.make_ticks_abs()
 events = []
 for track in midi_pattern:
     for event in track:
         events.append(event)
 
-bias_beats = True
-beat_biases = {(4, 4): (1.0, 0.9, 0.95, 0.875),
-               (3, 4): (1.0, 0.8, 0.9)}
-
-timesig = {}
-tempo = {}
+# filter events and convert to 
 events_by_tick = {}
-
-# filter and convert events
+timesig, tempo = {}, {}
 for event in events:
     if event.name == 'Time Signature':
         timesig[event.tick] = (event.numerator, event.denominator)
@@ -52,15 +59,17 @@ for event in events:
         else:
             events_by_tick[event.tick] = [note]
     elif event.name == 'Note Off':
-        note = [sequencer.STATUS_BYTES['NOTE_OFF'], note.pitch, 0]
+        note = [sequencer.STATUS_BYTES['NOTE_OFF'], event.pitch, 0]
         if event.tick in events_by_tick:
             events_by_tick[event.tick].append(note)
         else:
             events_by_tick[event.tick] = [note]
 
+# sort events by MIDI tick and calculate ticks between events
 events_list = sorted([(tick, group) for tick, group in events_by_tick.items()])
 delta_ticks = np.diff(list(zip(*events_list))[0])
 event_groups = [{'events': None, 'duration': 1.0}]
+# convert ticks to times based on tempo
 for i_group, group in enumerate(events_list):
     event_group = {'events': group[1]}
     tempo_i = tempo.get(group[0], 0)
@@ -69,19 +78,22 @@ for i_group, group in enumerate(events_list):
     try:
         event_group['duration'] = delta_ticks[i_group] * s_per_tick
     except IndexError:
-        event_group['duration'] = 5.0
+        event_group['duration'] = 1.0
     event_groups.append(event_group)
 
 # client with MIDI event sending only
 client = live.SynthInterfaceClient.from_synthname(synth_name,
-                                                  audiobuffer_time=5)
+                                                  audiobuffer_time=0.05)
 client.synth_config['reset'] = (0xB0, 0, 0)
 
-
+# capture and save synthesizer audio for the whole MIDI file
 with client:
     client.connect_synth()
-    client.capture_events(event_groups)
-
+    cProfile.run('client.capture_events(event_groups)', profile_path)
 buffers = client.drop_captured()
 snd = audio.buffers_to_snd(buffers)
-wavfile.write('/tmp/muser/goldberg.wav', 44100, snd)
+wavfile.write(wavfile_path, 44100, snd)
+
+# print profile statistics
+profile = pstats.Stats(profile_path).strip_dirs()
+profile.sort_stats('time').print_stats(10)
--- scripts/synth_tone.py
+++ scripts/synth_tone.py
@@ -1,31 +1,35 @@
+"""Basic test of tone synthesis with `muser.live.Synth`"""
+
 import muser.live as live
-import muser.utils as utils
 import math
 import time
 
-synth = live.SynthClient(channels=2)
-
-def sine_tone(amp, freq, phase=0):
-    def tone(t):
-        return amp * math.sin(2 * math.pi * freq * t + phase)
-    return tone
+synth = live.Synth(channels=2)
 
 def synth_on(synth, duration, pause=0):
+    """Activate synth's tone generation for a time, then pause."""
     synth.toggle()
     time.sleep(duration)
     synth.toggle()
     time.sleep(pause)
 
-synth.add_synth_function(sine_tone(0.75, 440))
+def sine_tone(amp, freq, phase=0):
+    """Returns a sinusoidal function of time."""
+    def tone(t):
+        return amp * math.sin(2 * math.pi * freq * t + phase)
+    return tone
+
 synth.activate()
 synth.connect(synth.outports[0], 'system:playback_1')
 synth.connect(synth.outports[1], 'system:playback_2')
 
+# add a 440 Hz tone and play twice for 1s, with a 0.5 pause between 
+synth.add_synth_function(sine_tone(0.75, 440))
 for i in range(2):
     synth_on(synth, duration=1, pause=0.5)
 
+# repeat, adding a tone an octave below
 synth.add_synth_function(sine_tone(0.75, 220))
-
 for i in range(2):
     synth_on(synth, duration=1, pause=0.5)
 
